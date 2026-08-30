4b97d43fe491126213281fa6f0da952ce10dd15d
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2026-08-29 20:29:07 -0600

    Make Delete file accept folder as an argument

--- Tables/delete_files.bat
+++ Tables/delete_files.bat
@@ -1,6 +1,19 @@
 @echo off
 
-del /Q ".\Tables\Raw_Downloads\*"
+if "%~1"=="" (
+    echo ERROR: Please provide a folder path.
+    echo Usage: %~nx0 "folder_path"
+    exit /b 1
+)
 
-echo Raw Downloads folder cleared.
-pause
+if not exist "%~1" (
+    echo ERROR: Folder does not exist: %~1
+    exit /b 1
+)
+
+echo Deleting files from: %~1
+
+del /Q "%~1\*"
+
+echo Done.
+
